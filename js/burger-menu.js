17288f5ef016761176045cb715362dad366e9c3d
vikashatalova <vika.shatalova.01@mail.ru>
2022-10-23 22:10:00 +0300

    burger-menu

---
+++ js/burger-menu.js
@@ -0,0 +1,32 @@
+const burgerMenuButton = document.querySelector('.burger-menu');
+const productListArrow = document.querySelector('.product-list__arrow');
+const productList = document.querySelector('.product-list__container');
+const productListOverlay = document.querySelector('.product-list__overlay');
+const productListHidden = document.querySelector('.product-list__hidden');
+
+burgerMenuButton.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (productList.style.display === 'none') {
+        productList.style.display = 'block'
+    } else {
+        productList.style.display = 'none'
+    }
+});
+
+productListArrow.addEventListener('click', (e) => {
+    e.preventDefault();
+    if (productListOverlay.style.display === 'none') {
+        productListOverlay.style.display = 'block'
+    } else {
+        productListOverlay.style.display = 'none'
+    }
+});
+
+// productListArrow.addEventListener('click', (e) => {
+//     e.preventDefault();
+//     if (productListHidden.style.display === 'none') {
+//         productListHidden.style.display = 'block'
+//     } else {
+//         productListHidden.style.display = 'none'
+//     }
+// })
